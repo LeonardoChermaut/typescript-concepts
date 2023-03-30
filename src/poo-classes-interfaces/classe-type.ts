b5b465060b5c353cbeec8897c087f2955fa4f7e3
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-03-30 09:58:12 -0300

    adjust: classes poo concepts

--- src/poo-classes-interfaces/classe-type.ts
+++ src/poo-classes-interfaces/classe-type.ts
@@ -2,35 +2,26 @@ export class Enterprise {
   constructor(
     public readonly name: string,
     protected readonly cnpj: string,
-    private employees: Employee[] = [],
+    private readonly employees: Employee[] = [],
   ) {}
 
   //SET
   setEmployee(employee: Employee): void {
-    const isEmployee = this.employees.find(
-      (e) => e.name === employee.name && e.lastName === employee.lastName,
-    );
+    const isEmployee = this.employees.find((e) => e.name === employee.name && e.lastName === employee.lastName);
 
     if (isEmployee) {
-      console.log(
-        `Employee ${employee.name} ${employee.lastName} already exists.`,
-      );
+      console.log(`Employee ${employee.name} ${employee.lastName} already exists.`);
     } else {
       this.employees.push(employee);
-      console.log(
-        '***************** CREATE EMPLOYEE *****************\nEmployee created!',
-      );
+      console.log(' CREATE EMPLOYEE \nEmployee created!');
     }
   }
 
   //GET
   getEmployeeName(name: string): void {
     this.employees.find((employee) => employee.name === name)
-      ? console.log(
-          `***************** GET NAME *****************\nEmployee: `,
-          employee,
-        )
-      : console.log(`Employee with name "${name}" not found.`);
+    ? console.log(` GET NAME \nEmployee: `, employee)
+    : console.log(`Employee with name "${name}" not found.`);
   }
 
   //DELETE
@@ -38,19 +29,20 @@ export class Enterprise {
     const index = this.employees.findIndex((employee) => employee.getId() === id);
     if (index !== -1) {
       this.employees.splice(index, 1);
-      console.log(`***************** DELETE EMPLOYEE *****************\nEmployee with ID ${id} deleted!`);
+      console.log(` DELETE EMPLOYEE \nEmployee with ID ${id} deleted!`);
     } else {
-      console.log(`***************** DELETE EMPLOYEE *****************\nEmployee with ID ${id} not found.`);
+      console.log(` DELETE EMPLOYEE \nEmployee with ID ${id} not found.`);
     }
   }
 
+  //UPDATE
   updateEmployee(id: number, employee: Employee): void {
     const index = this.employees.findIndex((empl) => empl.getId() === id);
     if (index !== -1) {
       this.employees[index].updateEmployee(employee.name, employee.lastName);
-      console.log(`***************** UPDATE EMPLOYEE *****************\nEmployee with ID ${id} updated!`);
+      console.log(` UPDATE EMPLOYEE \nEmployee with ID ${id} updated!`);
     } else {
-      console.log(`***************** UPDATE EMPLOYEE *****************\nEmployee with ID ${id} not found.`);
+      console.log(` UPDATE EMPLOYEE \nEmployee with ID ${id} not found.`);
     }
   }
 
@@ -69,6 +61,11 @@ export class Employee {
     return this.id;
   }
 
+  getEmployee(): string {
+    const employee = `${this.name} ${this.lastName}`;
+    return employee;
+  }
+
   updateEmployee(name?: string, lastName?: string): void {
     if (name && name !== this.name) {
       this.name = name;
@@ -78,16 +75,13 @@ export class Employee {
     }
   }
 
-  getEmployee(): string {
-    const employee = `${this.name} ${this.lastName}`;
-    return employee;
-  }
 }
 
 const employee = new Employee(1, 'Leonardo', 'Chermaut');
 const employeeTwo = new Employee(2, 'Abebe', 'Killah');
 enterprise.setEmployee(employee);
 enterprise.setEmployee(employeeTwo);
+
 
 //enterprise.deleteEmployee(1);
 enterprise.getEmployeeName('Leonardo');
