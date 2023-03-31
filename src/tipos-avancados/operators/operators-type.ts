37c76f702781bcff96a71a38fc36fc8e894e4e04
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-03-31 14:38:03 -0300

    add: opereradores de encadeamento opcional e coalescencia nula

---
+++ src/tipos-avancados/operators/operators-type.ts
@@ -0,0 +1,19 @@
+//Encadeamento opicional e Operador de coalescência nula
+
+type Doc = {
+  title: string;
+  text: string;
+  data?: Date;
+};
+
+const doc: Doc = {
+  title: 'Tyler the Creater',
+  text: 'Dont stop your focus',
+  data: new Date(),
+};
+
+
+//Se retornar null ou undefined é executado a operação do lado direito;
+console.log(doc.data?.toDateString() ?? 'You dont pass date');
+
+export {};
